ed3d6a249b2ac16064e9efd3329a13b82bec2a9c
NathanH2 <31360827+NathanH2@users.noreply.github.com>
2018-04-08 16:54:10 -0400

    Update 8ball.js

--- commands/8ball.js
+++ commands/8ball.js
@@ -5,7 +5,7 @@ module.exports.run = async (bot, message, args) =>{
     let replies = ["Yes.", "No.", "I'm not sure.", "Ask me again later."];
 
     let result = Math.floor((Math.random() * replies.length));
-    let question = args.slice(1).join(" ");
+    let question = args.slice(0).join(" ");
 
     let ballembed = new Discord.RichEmbed()
     .setAuthor(message.author.tag)
